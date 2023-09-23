29bd0d88258b0c0d447c73e3d95f176f33a40a21
WolverinDEV <git@did.science>
2023-09-23 14:05:39 +0200

    Testing auto release upload

---
+++ .github/workflows/discord_upload.sh
@@ -0,0 +1,6 @@
+git_commit_shash=$(git rev-parse --short "$GITHUB_SHA")
+git_branch=$(echo $GITHUB_REF | cut -d'/' -f 3)
+curl -H 'Content-Type: multipart/form-data' \
+  -X POST \
+  -F "payload_json={\"content\": \"Automated release build for $git_commit_shash on $git_branch\"}" \
+  -F "file=@../../README.MD" $DISCORD_RELEASE_WEBHOOK
