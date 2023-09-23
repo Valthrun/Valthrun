1c422a2ca8e2ebb018bef010585fae9c7a64a210
WolverinDEV <git@did.science>
2023-09-23 14:09:43 +0200

    Trying to update discord workflow

--- .github/workflows/discord_upload.sh
+++ .github/workflows/discord_upload.sh
@@ -3,4 +3,4 @@ git_branch=$(echo $GITHUB_REF | cut -d'/' -f 3)
 curl -H 'Content-Type: multipart/form-data' \
   -X POST \
   -F "payload_json={\"content\": \"Automated release build for $git_commit_shash on $git_branch\"}" \
-  -F "file=@../../README.MD" $DISCORD_RELEASE_WEBHOOK
+  -F "file=@README.MD" $DISCORD_RELEASE_WEBHOOK
