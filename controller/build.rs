419e91d831697f758b12f6e0c119bbdadc0466f8
Metaphysical <87896576+Metaphysical1@users.noreply.github.com>
2023-10-01 01:46:44 +0300

    Change default execution privileges
    always run as adminstrator

--- controller/build.rs
+++ controller/build.rs
@@ -5,6 +5,17 @@ use winres::WindowsResource;
 fn main() -> io::Result<()> {
     WindowsResource::new()
         .set_icon("../logo-icon.ico")
+        .set_manifest(r#"
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+        </security>
+        </trustInfo>
+        </assembly>
+        "#)
         .compile()?;
 
     Ok(())
