05225f7441cb59431c04f7bcfb7b26c3a9f8263f
WolverinDEV <git@mcgalaxy.de>
2023-10-01 15:46:05 +0200

    Updated the way of generating the app manifest

--- controller/build.rs
+++ controller/build.rs
@@ -2,15 +2,18 @@ use std::io;
 
 use winres::WindowsResource;
 
+const MANIFEST: &'static str = include_str!("./manifest.xml");
 fn main() -> io::Result<()> {
+    let execution_level = if cfg!(feature = "require-administrator") {
+        "requireAdministrator"
+    } else {
+        "asInvoker"
+    };
+
     let mut resource = WindowsResource::new();
     resource.set_icon("../logo-icon.ico");
-    #[cfg(feature = "exe-manifest")]
-    {
-        const MANIFEST: &'static str = include_str!("./manifest.xml");
-        resource.set_manifest(MANIFEST);
-    }
-
+    resource.set_manifest(&MANIFEST.replace("{{execution_level}}", execution_level));
     resource.compile()?;
+
     Ok(())
 }
