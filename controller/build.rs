6f231520905a44ac4893487795e904c2c6363502
WolverinDEV <git@mcgalaxy.de>
2023-10-06 19:56:09 +0200

    Dont manually generate an app manifest

--- controller/build.rs
+++ controller/build.rs
@@ -4,16 +4,16 @@ use winres::WindowsResource;
 
 const MANIFEST: &'static str = include_str!("./manifest.xml");
 fn main() -> io::Result<()> {
-    let execution_level = if cfg!(feature = "require-administrator") {
-        "requireAdministrator"
-    } else {
-        "asInvoker"
-    };
+    // let execution_level = if cfg!(feature = "require-administrator") {
+    //     "requireAdministrator"
+    // } else {
+    //     "asInvoker"
+    // };
 
-    let mut resource = WindowsResource::new();
-    resource.set_icon("../logo-icon.ico");
-    resource.set_manifest(&MANIFEST.replace("{{execution_level}}", execution_level));
-    resource.compile()?;
+    // let mut resource = WindowsResource::new();
+    // resource.set_icon("../logo-icon.ico");
+    // resource.set_manifest(&MANIFEST.replace("{{execution_level}}", execution_level));
+    // resource.compile()?;
 
     Ok(())
 }
