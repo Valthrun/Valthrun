13c655336e37dd6bb7139aab6dea32e3236cbbbb
WolverinDEV <git@mcgalaxy.de>
2023-10-06 20:33:19 +0200

    Bumped to version 0.3.0, updated cs2 schema and some code cleanup

--- controller/build.rs
+++ controller/build.rs
@@ -2,7 +2,6 @@ use std::io;
 
 use winres::WindowsResource;
 
-const MANIFEST: &'static str = include_str!("./manifest.xml");
 fn main() -> io::Result<()> {
     let mut resource = WindowsResource::new();
     resource.set_icon("../logo-icon.ico");
