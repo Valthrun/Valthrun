67b6b7e5cabb823a10b5c08bef4aa78da54d6f4a
WolverinDEV <git@did.science>
2023-10-19 15:20:13 +0200

    Updating the kernel interface to support the new driver features

--- kernel/interface/src/kinterface.rs
+++ kernel/interface/src/kinterface.rs
@@ -11,9 +11,19 @@ use std::{
 
 use valthrun_driver_shared::{
     requests::{
+        ControllerInfo,
+        DriverInfo,
         DriverRequest,
+        MemoryAccessMode,
+        RequestInitialize,
         RequestRead,
+        RequestReportSend,
+        RequestWrite,
         ResponseRead,
+        ResponseWrite,
+        INIT_STATUS_CONTROLLER_OUTDATED,
+        INIT_STATUS_DRIVER_OUTDATED,
+        INIT_STATUS_SUCCESS,
     },
     IO_MAX_DEREF_COUNT,
 };
@@ -39,6 +49,8 @@ use crate::{
 /// Interface for our kernel driver
 pub struct KernelInterface {
     driver_handle: Foundation::HANDLE,
+    driver_version: u32,
+
     read_calls: AtomicUsize,
 }
 
@@ -58,10 +70,60 @@ impl KernelInterface {
             .map_err(KInterfaceError::DeviceUnavailable)?
         };
 
-        Ok(Self {
+        let mut interface = Self {
             driver_handle,
+            driver_version: 0,
+
             read_calls: AtomicUsize::new(0),
-        })
+        };
+        interface.initialize()?;
+
+        Ok(interface)
+    }
+
+    fn initialize(&mut self) -> KResult<()> {
+        let controller_info = ControllerInfo {};
+        let mut driver_info = DriverInfo {};
+
+        let result = unsafe {
+            self.execute_request(&RequestInitialize {
+                target_version: 0x00_02_00_00,
+
+                controller_info: &controller_info,
+                controller_info_length: core::mem::size_of_val(&controller_info),
+
+                driver_info: &mut driver_info,
+                driver_info_length: core::mem::size_of_val(&driver_info),
+            })?
+        };
+
+        match result.status_code {
+            INIT_STATUS_SUCCESS => {}
+            INIT_STATUS_CONTROLLER_OUTDATED => {
+                return Err(KInterfaceError::DriverTooNew {
+                    driver_version: result.driver_version,
+                })
+            }
+            INIT_STATUS_DRIVER_OUTDATED => {
+                return Err(KInterfaceError::DriverTooOld {
+                    driver_version: result.driver_version,
+                })
+            }
+            status => return Err(KInterfaceError::InitializeInvalidStatus(status)),
+        };
+
+        self.driver_version = result.driver_version;
+        log::debug!(
+            "Successfully initialized kernel interface with driver version: {}.{}.{}",
+            (result.driver_version >> 24) & 0xFF,
+            (result.driver_version >> 16) & 0xFF,
+            (result.driver_version >> 8) & 0xFF
+        );
+        Ok(())
+    }
+
+    pub fn driver_version(&self) -> u32 {
+        self.driver_version
     }
 
     #[must_use]
@@ -135,6 +197,7 @@ impl KernelInterface {
              */
             self.execute_request::<RequestRead>(&RequestRead {
                 process_id,
+                mode: MemoryAccessMode::AttachProcess,
 
                 offsets: offset_buffer.clone(),
                 offset_count: offsets.len(),
@@ -164,6 +227,50 @@ impl KernelInterface {
                 })
             }
             ResponseRead::UnknownProcess => Err(KInterfaceError::ProcessDoesNotExists),
+            ResponseRead::AccessModeUnavailable => Err(KInterfaceError::RequestFailed),
+        }
+    }
+
+    #[must_use]
+    pub fn write<T: Copy>(&self, process_id: i32, address: u64, value: &T) -> KResult<()> {
+        let buffer = unsafe {
+            std::slice::from_raw_parts(
+                std::mem::transmute::<_, *mut u8>(value),
+                std::mem::size_of::<T>(),
+            )
+        };
+
+        self.write_slice(process_id, address, buffer)
+    }
+
+    #[must_use]
+    pub fn write_slice<T: Copy>(&self, process_id: i32, address: u64, buffer: &[T]) -> KResult<()> {
+        let result = unsafe {
+            self.execute_request(&RequestWrite {
+                process_id,
+                mode: MemoryAccessMode::AttachProcess,
+
+                address: address as usize,
+                buffer: buffer.as_ptr() as *const u8,
+                count: buffer.len() * core::mem::size_of::<T>(),
+            })
+        }?;
+
+        match result {
+            ResponseWrite::Success => Ok(()),
+            ResponseWrite::InvalidAddress => {
+                let mut offsets = [0; IO_MAX_DEREF_COUNT];
+                offsets[0] = address;
+                Err(KInterfaceError::InvalidAddress {
+                    target_address: address,
+                    resolved_offsets: [0; IO_MAX_DEREF_COUNT],
+                    resolved_offset_count: 0,
+                    offsets,
+                    offset_count: 1,
+                })
+            }
+            ResponseWrite::UnknownProcess => Err(KInterfaceError::ProcessDoesNotExists),
+            ResponseWrite::UnsuppportedAccessMode => Err(KInterfaceError::AccessModeUnavailable),
         }
     }
 
@@ -192,5 +299,19 @@ impl KernelInterface {
         }
 
         Ok(None)
+    }
+
+    pub fn add_metrics_record(&self, record_type: &str, record_payload: &str) -> KResult<()> {
+        unsafe {
+            self.execute_request(&RequestReportSend {
+                report_type: record_type.as_ptr(),
+                report_type_length: record_type.len(),
+
+                report_payload: record_payload.as_ptr(),
+                report_payload_length: record_payload.len(),
+            })
+        }?;
+
+        Ok(())
     }
 }
