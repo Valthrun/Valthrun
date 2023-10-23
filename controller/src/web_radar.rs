efb4a32647d94b75dcdd3b91af0268fa24894dfd
PetrSeifert <petr.seifert02@gmail.com>
2023-10-23 17:28:12 +0200

    Show HTML page for client

--- controller/src/web_radar.rs
+++ controller/src/web_radar.rs
@@ -2,11 +2,19 @@ use actix_web::{middleware::Logger, web, App, Error, HttpRequest, HttpResponse, 
 use actix_ws::Message;
 use futures_util::stream::StreamExt;
 
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(include_str!("web_radar/index.html"))
+}
 
 async fn ws(req: HttpRequest, body: web::Payload) -> Result<HttpResponse, Error> {
     let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
 
     actix_rt::spawn(async move {
+        // Send a message to the client
+        if session.text("Hello from the server!").await.is_err() {
+            return;
+        }
+
         while let Some(Ok(msg)) = msg_stream.next().await {
             match msg {
                 Message::Ping(bytes) => {
@@ -29,6 +37,7 @@ pub async fn run_server() -> Result<(), anyhow::Error> {
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
+            .route("/", web::get().to(index))
             .route("/ws", web::get().to(ws))
     })
         .bind("0.0.0.0:6969")?
