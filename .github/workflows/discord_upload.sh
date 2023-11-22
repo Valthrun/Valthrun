e7b9a4a732067f4fa894d17d2328fce3de244494
WolverinDEV <git@did.science>
2023-11-23 00:45:35 +0100

    Fixing discord file name

--- .github/workflows/discord_upload.sh
+++ .github/workflows/discord_upload.sh
@@ -6,4 +6,4 @@ git_branch=$(echo $GITHUB_REF | cut -d'/' -f 3)
 curl -H 'Content-Type: multipart/form-data' \
   -X POST \
   -F "payload_json={\"content\": \"Automated release build for $git_commit_shash on $git_branch\"}" \
-  -F "file=@$file_name; filename=$upload_prefix_$git_commit_shash.exe" $DISCORD_RELEASE_WEBHOOK
+  -F "file=@$file_name; filename=${upload_prefix}_${git_commit_shash}.exe" $DISCORD_RELEASE_WEBHOOK
