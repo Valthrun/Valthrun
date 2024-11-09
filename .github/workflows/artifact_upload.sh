1a35efa178e70eca931b44b3fc56291da8633188
WolverinDEV <git@did.science>
2024-11-09 16:32:11 +0900

    fix(ci): do not require versions to be signed on download

--- .github/workflows/artifact_upload.sh
+++ .github/workflows/artifact_upload.sh
@@ -23,7 +23,7 @@ fi
 echo "Uploading $file_path"
 curl -H "Content-Type:multipart/form-data" \
     -X POST \
-    -F "info={\"version\": \"$version\", \"versionHash\": \"$git_commit_shash\", \"requiresSigning\": true, \"updateLatest\": true }" \
+    -F "info={\"version\": \"$version\", \"versionHash\": \"$git_commit_shash\", \"updateLatest\": true }" \
     -F "payload=@$file_path; filename=${artifact}_${git_commit_shash}.${file_name##*.}" \
     "https://valth.run/api/artifacts/$artifact/$artifact_track?api-key=$ARTIFACT_API_KEY" || exit 1
 
