4a4b03a594c8c1a6dc25e3c37432988e76554724
WolverinDEV <git@did.science>
2024-11-10 23:06:34 +0900

    fix(radar-server): rerenable all radar maps

--- radar/web/src/map-info/index.ts
+++ radar/web/src/map-info/index.ts
@@ -13,19 +13,19 @@ export type MapStyle = {
 }
 
 export const kRegisteredMaps: Record<string, () => Promise<LoadedMap>> = {
-    // cs_italy: () => import("./cs_italy").then((value) => value.default),
-    // cs_office: () => import("./cs_office").then((value) => value.default),
-    // de_ancient: () => import("./de_ancient").then((value) => value.default),
-    // de_anubis: () => import("./de_anubis").then((value) => value.default),
-    // de_cache: () => import("./de_cache").then((value) => value.default),
-    // de_dust2: () => import("./de_dust2").then((value) => value.default),
-    // de_inferno: () => import("./de_inferno").then((value) => value.default),
-    // de_mills: () => import("./de_mills").then((value) => value.default),
-    // de_mirage: () => import("./de_mirage").then((value) => value.default),
-    // de_nuke: () => import("./de_nuke").then((value) => value.default),
-    // de_overpass: () => import("./de_overpass").then((value) => value.default),
-    // de_thera: () => import("./de_thera").then((value) => value.default),
-    // de_train: () => import("./de_train").then((value) => value.default),
+    cs_italy: () => import("./cs_italy").then((value) => value.default),
+    cs_office: () => import("./cs_office").then((value) => value.default),
+    de_ancient: () => import("./de_ancient").then((value) => value.default),
+    de_anubis: () => import("./de_anubis").then((value) => value.default),
+    de_cache: () => import("./de_cache").then((value) => value.default),
+    de_dust2: () => import("./de_dust2").then((value) => value.default),
+    de_inferno: () => import("./de_inferno").then((value) => value.default),
+    de_mills: () => import("./de_mills").then((value) => value.default),
+    de_mirage: () => import("./de_mirage").then((value) => value.default),
+    de_nuke: () => import("./de_nuke").then((value) => value.default),
+    de_overpass: () => import("./de_overpass").then((value) => value.default),
+    de_thera: () => import("./de_thera").then((value) => value.default),
+    de_train: () => import("./de_train").then((value) => value.default),
     de_vertigo: () => import("./de_vertigo").then((value) => value.default),
 };
 
