5b4f5ff875316ce03127db18ba8e0fbc87ee2ab4
WolverinDEV <git@did.science>
2025-03-09 21:24:49 +0300

    fix(ci): fallback to default rustc version if VT_RUSTC has not been set

--- .github/workflows/vt-rustc-setup.ps1
+++ .github/workflows/vt-rustc-setup.ps1
@@ -5,26 +5,32 @@ if ($LastExitCode -ne 0) {
     throw "failed to install nightly toolchain"
 }
 
-# Download the rustc compiler
-Write-Host "Downloading Valthrun rustc"
-Invoke-WebRequest $env:VT_RUSTC -OutFile "vt_rustc.zip" -Headers @{
-    "Accept"               = "application/octet-stream"
-    "Authorization"        = "Bearer $env:VT_RUSTC_AUTHORIZATION"
-    "X-GitHub-Api-Version" = "2022-11-28"
+if ($env:VT_RUSTC -ne "") {
+    # Download the rustc compiler
+    Write-Host "Downloading Valthrun rustc"
+    Invoke-WebRequest $env:VT_RUSTC -OutFile "vt_rustc.zip" -Headers @{
+        "Accept"               = "application/octet-stream"
+        "Authorization"        = "Bearer $env:VT_RUSTC_AUTHORIZATION"
+        "X-GitHub-Api-Version" = "2022-11-28"
+    }
+
+    Write-Host "Extracting Valthrun rustc"
+    Expand-Archive -Path "vt_rustc.zip" -DestinationPath "$(Get-Location)/vt_rustc" -Force
+
+    Write-Host "Linking Valthrun rustc"
+    rustup toolchain link vt-rust "$(Get-Location)/vt_rustc"
+    if ($LastExitCode -ne 0) {
+        throw "failed to link Valthrun rust version"
+    }
+
+    rustup default vt-rust
+    if ($LastExitCode -ne 0) {
+        throw "failed to default to the Valthrun rust version"
+    }
 }
-
-Write-Host "Extracting Valthrun rustc"
-Expand-Archive -Path "vt_rustc.zip" -DestinationPath "$(Get-Location)/vt_rustc" -Force
-
-Write-Host "Linking Valthrun rustc"
-rustup toolchain link vt-rust "$(Get-Location)/vt_rustc"
-if ($LastExitCode -ne 0) {
-    throw "failed to link Valthrun rust version"
-}
-
-rustup default vt-rust
-if ($LastExitCode -ne 0) {
-    throw "failed to default to the Valthrun rust version"
+else {
+    Write-Host "Valthrun rustc download URL not set. Using standard rustc version."
+    # No need to setup anything then
 }
 
 rustc -V -v
